075c20035b6a10898842ea7418fa3fc69d1c5915
xiaYuTian11 <38251546+xiaYuTian11@users.noreply.github.com>
2022-10-17 16:18:26 +0800

    添加 configs 模块

---
+++ efficient-boot-configs-start/src/main/java/com/efficient/configs/properties/ConfigsProperties.java
@@ -0,0 +1,13 @@
+package com.efficient.configs.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author TMW
+ * @since 2022/10/17 15:56
+ */
+@ConfigurationProperties("com.efficient.configs")
+@Data
+public class ConfigsProperties {
+}
